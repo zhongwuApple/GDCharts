a3b404a0e030563120701b495b0c0ff2003e0fc2
tanzhongwu <751890720@qq.com>
2018-10-22 18:39:45 +0800

    first commit

---
+++ GDCharts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDCharts
+//
+//  Created by tiger on 2018/10/22.
+//  Copyright © 2018 tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GDCharts/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GDCharts
+//
+//  Created by tiger on 2018/10/22.
+//  Copyright © 2018 tiger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
